60079e07580160898ce16eb85c3edefccf93e73d
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2022-01-18 19:17:44 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import styles from "./App.module.scss";
+
+// Pages
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Character from "./pages/Character/Character";
+import Characters from "./pages/Characters/Characters";
+import Episode from "./pages/Episode/Episode";
+import Episodes from "./pages/Episodes/Episodes";
+import Location from "./pages/Location/Location";
+import Locations from "./pages/Locations/Locations";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/characters" element={<Characters />} />
+        <Route path="/characters/:id" element={<Character />} />
+
+        <Route path="/episodes" element={<Episodes />} />
+        <Route path="/episodes/:id" element={<Episode />} />
+
+        <Route path="/locations" element={<Locations />} />
+        <Route path="/locations/:id" element={<Location />} />
+      </Routes>
     </div>
   );
 }
