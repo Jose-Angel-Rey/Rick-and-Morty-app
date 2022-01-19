9ead5f9bb1ba50229e33fef50708ee70c8771c7c
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2022-01-18 20:30:42 -0400

    Update Locations.tsx

--- src/pages/Locations/Locations.tsx
+++ src/pages/Locations/Locations.tsx
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { useLocations } from "../../graphql/customHooks";
+import Pagination from "../../components/Pagination/Pagination";
 
 export default function Locations() {
-  const { loading, error, data } = useLocations();
+  const [page, setPage] = useState(1);
+  const { loading, error, data } = useLocations(page);
+
+  console.log("location", data?.locations?.info);
   return (
     <div>
       Locations
       <hr />
       <br />
       {loading && <p>Loading...</p>}
-      {error && <p>Error :(</p>}
+      {error && <p>Error...</p>}
       {data && (
         <ul>
           {data.locations.results.map((location: any) => (
@@ -20,6 +25,12 @@ export default function Locations() {
           ))}
         </ul>
       )}
+      <Pagination
+        page={page}
+        setPage={setPage}
+        hasNextPage={typeof data?.locations?.info?.next === "number"}
+        hasPreviousPage={typeof data?.locations?.info?.prev === "number"}
+      />
     </div>
   );
 }
