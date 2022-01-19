eb66a5638f5f38d7930a02f05104e8986f286239
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2022-01-19 07:41:54 -0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import styles from "./App.module.scss";
 
-// Pages
-import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Character from "./pages/Character/Character";
 import Characters from "./pages/Characters/Characters";
@@ -10,11 +8,11 @@ import Episode from "./pages/Episode/Episode";
 import Episodes from "./pages/Episodes/Episodes";
 import Location from "./pages/Location/Location";
 import Locations from "./pages/Locations/Locations";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className={styles.App}>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
 
@@ -27,6 +25,8 @@ function App() {
         <Route path="/locations" element={<Locations />} />
         <Route path="/locations/:id" element={<Location />} />
       </Routes>
+
+      <Footer />
     </div>
   );
 }
